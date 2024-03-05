ec4b1d3a5913c9bf41012aee973841251d60961e
Gaussian <33029537+GaussianGuaicai@users.noreply.github.com>
2024-03-05 15:35:18 +0800

    Merge and modifed lights import implementation from joeyskeys/light_schema (08f95cd7207033cc860a28f8fff37c8b95131500)

---
+++ package/com.unity.formats.usd/Runtime/Scripts/IO/Geometry/LightImporter.cs
@@ -0,0 +1,20 @@
+
+
+using pxr;
+using UnityEngine;
+using USD.NET.Unity;
+
+namespace Unity.Formats.USD
+{
+	public static class LightImporter<T> where T : LightSampleBase
+	{
+		public static void BuildLight(T usdLight,
+			GameObject go,
+			UsdPrim usdPrim,
+			SceneImportOptions options)
+		{
+			var light = ImporterBase.GetOrAddComponent<Light>(go);
+			usdLight.CopyToLight(light,usdPrim, setTransform: false);
+		}
+	}
+}
